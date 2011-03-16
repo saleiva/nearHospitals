52c0f98c04439905ee79a6de0f7a073a539fe389
saleiva <saleiva@gmail.com>
2011-03-16 17:18:11 +0100

    railsapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NearHospitals::Application.config.secret_token = 'd483a419e781d0c88e28293bd03e6687621c822d621aa9dd8ffea878a264c95d38edf28e50a90319bae6023eb9215ffa2587476ee1c627edc8759a592ecde4ad'
