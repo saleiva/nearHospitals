76dbe805d352de0d01e4a64332fa7a116892a84d
Fernando Espinosa <fer@ferdev.com>
2011-03-18 14:22:09 +0100

    Moves hospitals list to a new partial

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
   def index
-    records = CartoDB::Connection.records "near_hospitals_#{Rails.env}"
-    @hospitals = records.rows
+    @hospitals = []
+    if request.xhr? && params[:lat].present? && params[:lon].present?
+      records = CartoDB::Connection.records "near_hospitals_#{Rails.env}"
+      @hospitals = records.rows
+      render :partial => 'hospitals' and return
+    end
   end
 end
