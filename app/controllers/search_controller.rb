0d49ae78c37f0fe24ddb03e0d8e7ac3946ea1bd1
Fernando Espinosa <fer@ferdev.com>
2011-03-18 14:34:53 +0100

    Gets all hospitals

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @hospitals = []
     if request.xhr? && params[:lat].present? && params[:lon].present?
-      records = CartoDB::Connection.records "near_hospitals_#{Rails.env}"
+      records = CartoDB::Connection.query "SELECT * FROM near_hospitals_#{Rails.env}"
       @hospitals = records.rows
       render :partial => 'hospitals' and return
     end
