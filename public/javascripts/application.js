a371f07383f2cc021c8a686022ed891257e0f01d
Javier Álvarez Medina <xavijam@gmail.com>
2011-03-20 01:22:51 +0100

    change in maps

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,6 +4,13 @@
     var actual_position;
     var request;
     var hospital_array = [];
+    var mapOptions = {
+      zoom: 4,
+      center: new google.maps.LatLng(-33, 151),
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      draggable: false
+    }
 
     window.onload = function(){
       refreshPosition();
@@ -11,7 +18,7 @@
     
     function refreshPosition() {
       $('span.loader p').text('Obteniendo tu posición...');
-      $('div.box').fadeOut('fast');
+      $('div.box').animate({opacity:0},200);
       $('span.loader').fadeIn('slow',function(){
         if(navigator.geolocation){
           navigator.geolocation.getCurrentPosition(handleGetCurrentPosition, handleGetCurrentPositionError);
@@ -27,21 +34,17 @@
         count++;
         if (count == hospitals.length) {
           $('span.loader').fadeOut(function(){
-            
-            
             var sortedKeys = new Array();
             var sortedObj = {};
             for (var i in hospital_array){
               sortedKeys.push({v:i,c:hospital_array[i]});
             }
             sortedKeys.sort(function(x,y){return y.c - x.c});
-            
             for (var i in sortedKeys) {
               var element = $('#hospital_'+sortedKeys[i].v).parent().parent();
               element.insertAfter('div#box');
             }
-            
-            $('div.box').fadeIn();
+            $('div.box').animate({opacity:1},500);
           });
         }
       });
@@ -61,23 +64,37 @@
             $(document).trigger('load');
             if (status == google.maps.DirectionsStatus.OK) {
               var steps = response.routes[0].legs[0].steps;
-              var path = '';
+              var path = [];
               for (var i=0; i<steps.length; i++) {
-                path += steps[i].path[0].lat().toFixed(4) + "," + steps[i].path[0].lng().toFixed(4) + '|';
+                for (var j=0; j<steps[i].path.length;j++) {
+                  path.push(steps[i].path[j]);
+                }
               }
-              path = path.substr(0,path.length-2);
-
+              var map = new google.maps.Map(document.getElementById('hospital_'+hospital['cartodb_id']),mapOptions);
+              var poly_options = {'strokeWeight':'3','strokeColor':'#070707'} ;
+        	    var newPoly = new google.maps.Polyline(poly_options);
+        	    newPoly.setPath(path);
+        	    newPoly.setMap(map);
+        	    var bounds = new google.maps.LatLngBounds();
+        	    bounds.extend(path[0]);
+        	    bounds.extend(path[path.length-1]);
+        	    map.fitBounds(bounds);
+        	    map.setZoom(map.getZoom()+1);
+        	    var marker = new google.maps.Marker({
+                  position: path[0], 
+                  map: map,
+                  label: "S"
+              });
+              var marker = new google.maps.Marker({
+                  position: path[path.length-1], 
+                  map: map
+              });
+              
               var start_address = response.routes[0].legs[0].start_address.replace( / /g,'+');
               var end_address = response.routes[0].legs[0].end_address.replace( / /g,'+');
-              
-              
               hospital_array[hospital['cartodb_id']] = response.routes[0].legs[0].distance.value;
-
               $('#hospital_'+hospital['cartodb_id']).parent().attr('href','http://maps.google.com/maps?saddr='+start_address+"&daddr="+end_address);
               $('#hospital_'+hospital['cartodb_id']).parent().parent().children('p.distance').text(response.routes[0].legs[0].duration.text + ' / ' + response.routes[0].legs[0].distance.text);
-              $('#hospital_'+hospital['cartodb_id']).attr('src',"http://maps.google.com/maps/api/staticmap?path=color:0xff3300ff|weight:3|"+path+"&size=240x150"+
-              "&markers=color:blue|"+actual_position.lat()+","+actual_position.lng()+"&markers=color:green|label:H|"+
-              response.Gf.destination.lat()+","+response.Gf.destination.lng()+"&sensor=false");
             }
           });
         });
